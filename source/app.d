5c1a1ac3eb2f56ca7692c69ee412c9ca90f4b28e
Michal Hojgr <michal.hojgr@gmail.com>
2015-01-31 16:27:37 +0100

    Routing: Improve assignRoutes so it takes Controller type only once

--- source/app.d
+++ source/app.d
@@ -6,8 +6,9 @@ import std.conv;
 import router;
 import annotation;
 
-template assignRoutes(ControllerType) {
-    Router eval(ControllerType ctrl, Router router) {
+template assignRoutes(alias ctrl) {
+    alias ControllerType = typeof(ctrl);
+    Router eval(Router router) {
         foreach(member; __traits(allMembers, ControllerType)) {
             static if(hasAnnotation!(__traits(getMember, ControllerType, member), Route) == true) {
                 Route r = getAnnotation!(__traits(getMember, ctrl, member), Route);
@@ -24,12 +25,12 @@ void main() {
     auto settings = new v.HTTPServerSettings;
     settings.bindAddresses = ["127.0.0.1"];
     settings.port = 8080;
-    
+
     Router router = new Router;
 
     IndexController index = new IndexController;
 
-    assignRoutes!(typeof(index)).eval(index, router);
+    assignRoutes!(index).eval(router);
 
     v.listenHTTP(settings, router);
     v.runEventLoop();
